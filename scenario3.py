fb8fdaae097e46f0facd440901e59c75f31c6df7
Dacio Romero <dacioromero@gmail.com>
2019-05-09 12:10:08 -0700

    Add Trie pickler and load pickled Tries in scenario 3

--- scenario3.py
+++ scenario3.py
@@ -1,13 +1,14 @@
 from trie import Trie
 from data_reader import routes_gen, numbers_gen
-
+import pickle
 
 def main():
-  print('Growing Trie')
-  trie = Trie(routes_gen('10000000'))
-  print('Trie grown')
+  # print('Growing Trie')
+  # trie = Trie(routes_gen('1000000'))
+  # print('Trie grown')
+  trie = pickle.load(open('10000000.trie', 'rb'))
 
-  for number in numbers_gen('1000'):
+  for number in numbers_gen('100'):
     print(number, trie.find_closest(number), sep=', ')
 
 if __name__ == '__main__':
