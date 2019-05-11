171f4727011f5023940102953d10481578aeda00
Dacio Romero <dacioromero@gmail.com>
2019-05-11 16:44:48 -0700

    Optimize compact_trie

--- compact_trie.py
+++ compact_trie.py
@@ -1,42 +1,62 @@
 from typing import Dict, Optional, Any, Tuple, Iterable
-from utils import find_unshared
 from trie import Trie, TrieNode
 
+def find_uncommon_index(s1: str, s2: str) -> int:
+  i = 0
+  bounds = min(len(s1), len(s2))
+
+  while i < bounds:
+    if s1[i] != s2[i]:
+      break
+
+    i += 1
+
+  return i
+
+# TODO: Make find_closest and insert reuse more code
 class CompactTrie(Trie):
   def find_closest(self, key: str) -> Any:
     node = self.root
     cur_value = None
 
     while node is not None:
-      # Node with key exists -> return value
+      # Node with key exists
       if key in node.children:
-        return node.children[key]
+        # Return value of child
+        return node.children[key].value
+
+      len_key = len(key)
 
       for prefix in node.children:
-        # Skip non-prefixes
-        if len(prefix) > len(key):
+        len_prefix = len(prefix)
+
+        # Non-prefixes
+        if len_prefix > len_key or key[0] != prefix[0]:
           continue
 
-        # Find the first unmatching index
-        split_index = find_unshared(key, prefix)
+        uncommon_index = find_uncommon_index(key, prefix)
 
-        # No match -> continue searching
-        if split_index == 0:
-          continue
-        # Equivalent to key.startswith(prefix) -> continue searching within this prefix
-        elif split_index == len(prefix):
+        # Equivalent to key.startswith(prefix)
+        if uncommon_index == len_prefix:
+          # Search child node for matches
           node = node.children[prefix]
+
+          # Store value for returning later
           if node.value is not None:
             cur_value = node.value
 
           # Strip key of match
-          key = key[split_index:]
+          key = key[uncommon_index:]
+
+          # Stop searching old prefixes
           break
-        # Partial match -> return last match
+        # Partial match
         else:
+          # Return last match
           return cur_value
-      # No full or partial matches -> return last match
+      # No potential prefixes found
       else:
+        # Return last match
         return cur_value
 
     return cur_value
@@ -44,48 +64,58 @@ class CompactTrie(Trie):
   def insert(self, key: str, value: Any) -> None:
     node = self.root
 
-    while 0 < len(key):
+    while key:
       # Node with key already exists, set value
       if key in node.children:
         node.children[key].value = value
         return
 
+      len_key = len(key)
+
       for prefix in node.children:
-        # Skip non-prefixes
-        if len(prefix) > len(key):
+        len_prefix = len(prefix)
+
+        # Non-prefixes
+        if len_prefix > len_key or key[0] != prefix[0]:
           continue
 
-        # Find the first unmatching index
-        split_index = find_unshared(key, prefix)
+        uncommon_index = find_uncommon_index(key, prefix)
 
-        # No match -> continue searching for matching prefixes
-        if split_index == 0:
-          continue
-        # Equivalent to key.startswith(prefix) -> continue searching within this prefix
-        elif split_index == len(prefix):
+        # Equivalent to key.startswith(prefix)
+        if uncommon_index == len_prefix:
+          # Search child node for matches
           node = node.children[prefix]
 
           # Strip key of match
-          key = key[split_index:]
+          key = key[len_prefix:]
+
+          # Stop searching old prefixes
           break
-        # Partial match -> split
+        # Partial match
         else:
+          # Create new node to split previous values and new
           new_parent = TrieNode()
 
-          # Add prev values keyed with non-matching old prefix substring
-          new_parent.children[prefix[split_index:]] = node.children.pop(prefix)
-          # Add new value keyed with non-matching key substring
-          new_parent.children[key[split_index:]] = TrieNode(value)
+          # Split off new key and prefix for prev values
+          split_prefix, split_key = prefix[:uncommon_index], prefix[uncommon_index:]
+
+          # Move prev node to its new key on parent
+          new_parent.children[split_key] = node.children.pop(prefix)
+          # Create new node with value with substring of non-matching key
+          new_parent.children[key[uncommon_index:]] = TrieNode(value)
 
           # Add parent keyed with matching string
-          node.children[prefix[:split_index]] = new_parent
+          node.children[split_prefix] = new_parent
 
+          # Node inserted, exit
           return
-      # No full or partial matches -> create new node with key
+      # No potential prefixes found
       else:
+        # Create new node with value with key
         node.children[key] = TrieNode(value)
-        return
 
+        # Node inserted, exit
+        return
 
 if __name__ == "__main__":
   trie = CompactTrie()
