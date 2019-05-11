4af42fda99b1fccc0b1eb4182f8b854766f1ed60
Dacio Romero <dacioromero@gmail.com>
2019-05-11 16:45:13 -0700

    Update scenario3

--- scenario3.py
+++ scenario3.py
@@ -1,19 +1,19 @@
-from memory_profiler import memory_usage
-# from memory_profiler import profile
-
 from trie_serializer import load
 from data_reader import numbers_gen
 
-# @profile
 def main():
   trie = load('10000000')
 
+  print('Writing results to out.txt...')
+
   with open('out.txt', 'w') as f:
-    for number in numbers_gen('1000'):
+    for number in numbers_gen('10000'):
       f.write(number)
       f.write(', ')
-      f.write(str(trie.find_closest(number)))
+      f.write(str(trie.find_closest(number) or 0))
       f.write('\n')
 
+  print('Finished!')
+
 if __name__ == '__main__':
-  print(max(memory_usage(proc=main)))
+  main()
