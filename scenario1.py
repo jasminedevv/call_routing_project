8a8729a6852d596e508ab55a9b7d26ed6493c1ac
Dacio Romero <dacioromero@gmail.com>
2019-05-15 15:51:01 -0700

    Make scenario 1's function consistent

--- scenario1.py
+++ scenario1.py
@@ -5,7 +5,7 @@ from utils import decode_route_prefix
 routes = list(decode_route_prefix(routes_gen('106000')))
 routes.sort(key=lambda r: len(r[0]), reverse=True)
 
-def get_cost(number: str) -> float:
+def get_price(number: str) -> float:
   """Finds the price of a number by searching for the longest corresponding route."""
   for prefix, cost in routes:
     if number.startswith(prefix):
@@ -17,6 +17,6 @@ if __name__ == '__main__':
   from random import choice
 
   number = choice(list(numbers_gen('1000'))).decode()
-  cost = get_cost(number)
+  cost = get_price(number)
 
   print(number, cost, sep=', ')
